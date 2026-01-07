327ca85b218fe3ecb209eaf1fe35ec788d5f232c
DeathScythe1021 <jenny90yeh@gmail.com>
2026-01-07 13:14:07 +0800

    add: create enter animation

--- career_card/script.js
+++ career_card/script.js
@@ -1,6 +1,17 @@
-//ScrollTrigger
-import gsap from "gsap"; 
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+
+
 gsap.registerPlugin(ScrollTrigger);
 
-document.addEventListener("")
+// 選取卡片容器
+const cardWrapper = document.querySelector(".card-wrapper");
+
+// 進場動畫
+gsap.from(cardWrapper, {
+  y: 50,          
+  opacity: 0,     
+  rotationX: -20, 
+  duration: 1.2,
+  ease: "power3.out",
+  delay: 0.2     
+});
