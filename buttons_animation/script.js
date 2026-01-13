cc91ec8d722ba22d75c0fb1c18fa59631bf4cd6c
DeathScythe1021 <jenny90yeh@gmail.com>
2026-01-13 20:58:52 +0800

    feat: add black star animation

---
+++ buttons_animation/script.js
@@ -0,0 +1,20 @@
+const circle = document.querySelector(".circle");
+
+//黑色星星按鈕動畫
+const blackStar=
+    gsap.from(
+    ".black-star",{
+        rotation:"+=360",
+        duration:2,
+        repeat: -1,
+        paused:true
+});
+circle.addEventListener("mouseenter",()=>{
+    blackStar.play();
+
+});
+
+circle.addEventListener("mouseleave",()=>{
+    blackStar.pause();
+    blackStar.progress(0);//動畫時間線歸0（回到初始位置）
+});
