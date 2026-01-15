6241a2a7c7a83857cf5636e2a1ab0a7a2a4eb56b
DeathScythe1021 <jenny90yeh@gmail.com>
2026-01-15 17:36:36 +0800

    feat: add brackets animation

--- buttons_animation/script.js
+++ buttons_animation/script.js
@@ -1,5 +1,5 @@
 // CDN置入
-
+gsap.registerPlugin(TextPlugin);
 
 // 變數
 const circle = document.querySelector(".circle");
@@ -57,3 +57,36 @@ tl.pause();
     overwrite: true   // 重要：確保不要跟之前的動畫打架
   });
 });
+
+//括號動畫
+const container = document.querySelector(".bracket-container");
+const middleText = container.querySelector(".b-text");
+
+// 文字分割
+const textSplit = new SplitType(middleText, { types: 'chars' });
+// 隱藏字母
+gsap.set(textSplit.chars, { opacity: 0 });
+
+const tl2 = gsap.timeline({ 
+  paused: true,
+  defaults: { duration: 0.5, ease: "power2.out" }
+});
+
+tl2.to(middleText, {
+  width: "auto",      // 物理撐開，推動右邊長條
+  opacity: 1,         // 文字浮現
+  marginLeft: "15px", // 動畫開始時才加入間距
+  marginRight: "15px"
+});
+
+// 打字動畫
+tl2.to(textSplit.chars, {
+    
+  opacity: 1,
+  duration: 0,  // 每個字浮現的時間 (打字通常很快)
+  stagger: 0.1,   // 每個字之間的間隔 (打字節奏)
+}, "<0.2");
+
+// 綁定滑鼠事件
+container.addEventListener("mouseenter", () => tl2.play());
+container.addEventListener("mouseleave", () => tl2.reverse());
